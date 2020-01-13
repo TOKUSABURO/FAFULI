f01b80e48b0ff84ed13da3689dcaad98a17ce077
MUJAWAMARIYA <mumarg2015@gmail.com>
2020-01-13 10:52:16 +0200

    course table of first free project @tech

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_13_084850) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "courses", force: :cascade do |t|
+    t.string "info_title"
+    t.string "info_details"
+    t.string "free_info_topics"
+    t.string "free_info_details"
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
