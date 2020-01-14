c37558dec985a753379e8bcb6dc75b7c4e251675
MUJAWAMARIYA <mumarg2015@gmail.com>
2020-01-14 17:52:02 +0200

    making sign_up and log_in design

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
 get 'home/index'
+
   devise_for :users
   resources :courses
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -7,11 +9,8 @@ get 'home/index'
   get 'comments/create'
   root 'home#index'
   resources :purchases
-
-
   resources :courses do
     resources :comments
   end
     # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
 end
