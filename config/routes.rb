8bb13b559f626656f6b87b32127c170d9e8e133e
AimableHonorable <aimatwiri@gmail.com>
2020-01-14 14:46:56 +0200

    homepage design

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 get 'home/index'
   devise_for :users
   resources :courses
-  root 'courses#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'comments/create'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_125447) do
+ActiveRecord::Schema.define(version: 2020_01_13_133005) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(version: 2020_01_13_125447) do
     t.string "free_info_topics"
     t.string "free_info_details"
     t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "purchases", force: :cascade do |t|
+    t.integer "ammount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -59,13 +65,6 @@ ActiveRecord::Schema.define(version: 2020_01_13_125447) do
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-
-  create_table "purchases", force: :cascade do |t|
-    t.integer "ammount"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "comments", "courses"
