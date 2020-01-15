1db152d25140a1c4c69bf6afe8a716b369bcc6a3
kavunap <pkavuna@gmail.com>
2020-01-15 13:21:07 +0200

    Oauth

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+    #make sure to override devise 
     def build_resource(hash={})
       hash[:uid] = User.create_unique_string
       super
