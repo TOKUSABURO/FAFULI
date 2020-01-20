34bbd7d12717f7b249bdef1713d3872051658ce0
Adali12 <abayiringira1994@yahoo.com>
2020-01-20 01:35:58 -0800

    adding payment form on course

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'users/show'
  get 'home/index'
   resources :courses
+  resources :charges
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'comments/create'
   root 'home#index'
