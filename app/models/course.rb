5522ba3b7813c7f2a46abcb66fd0feeae202b85d
MUJAWAMARIYA <mumarg2015@gmail.com>
2020-01-23 13:43:31 +0200

    adding validation to course and comment table

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,12 @@
 class Course < ApplicationRecord
     has_many :comments, dependent: :destroy
+
+
+    validates :info_title, :info_details, :free_info_topics, :free_info_details,  presence: true, length: { minimum: 5}
+
+   
+    validates :info_title, :info_details, :free_info_topics, :free_info_details,  format: { with: /\A[a-zA-Z'-]*\z/ }, length: { maximum: 25}
+  validates :price, presence:true
+   
+
 end
