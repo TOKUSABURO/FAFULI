2d8cbf954e550b05c152e953033bf026d8f7fcdf
AimableHonorable <aimatwiri@gmail.com>
2020-01-27 14:43:14 +0200

    login with facebook

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_01_23_180845) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentication_providers", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_name_on_authentication_providers"
-  end
 
   create_table "comments", force: :cascade do |t|
     t.string "content"
@@ -51,7 +43,6 @@ ActiveRecord::Schema.define(version: 2020_01_23_180845) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_courses_on_user_id"
-
   end
 
   create_table "identities", force: :cascade do |t|
@@ -69,7 +60,6 @@ ActiveRecord::Schema.define(version: 2020_01_23_180845) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_identities_on_user_id"
-
   end
 
   create_table "messages", force: :cascade do |t|
@@ -87,39 +77,6 @@ ActiveRecord::Schema.define(version: 2020_01_23_180845) do
     t.integer "ammount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "sessions", force: :cascade do |t|
-    t.string "session_id", null: false
-    t.string "data"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["session_id"], name: "index_sessions_on_session_id", unique: true
-    t.index ["updated_at"], name: "index_sessions_on_updated_at"
-  end
-
-  create_table "social_accounts", force: :cascade do |t|
-    t.string "token"
-    t.string "secret"
-    t.bigint "user_id"
-    t.bigint "authentication_provider_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["authentication_provider_id"], name: "index_social_accounts_on_authentication_provider_id"
-    t.index ["user_id"], name: "index_social_accounts_on_user_id"
-  end
-
-  create_table "user_authentications", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "authentication_provider_id"
-    t.string "uid"
-    t.string "token"
-    t.datetime "token_expires_at"
-    t.text "params"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["authentication_provider_id"], name: "index_user_authentications_on_authentication_provider_id"
-    t.index ["user_id"], name: "index_user_authentications_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -146,20 +103,16 @@ ActiveRecord::Schema.define(version: 2020_01_23_180845) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-
-    t.string "uid", limit: 500, default: "", null: false
-    t.string "provider", limit: 50, default: "", null: false
-]
+    t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "identities", "users"
+  add_foreign_key "comments", "courses"
   add_foreign_key "comments", "users"
   add_foreign_key "courses", "users"
+  add_foreign_key "identities", "users"
   add_foreign_key "messages", "conversations"
   add_foreign_key "messages", "users"
-  add_foreign_key "social_accounts", "authentication_providers"
-  add_foreign_key "social_accounts", "users"
 end
