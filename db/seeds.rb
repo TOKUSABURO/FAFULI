1f04320cbc5ce207e8347d91f02b320b274a3451
kavunap <pkavuna@gmail.com>
2020-01-31 09:35:06 +0200

    debuged

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name: 'user', email: 'user@gmail.com', user_type: 'teacher', admin: 'true', 
+hobbies: 'programming', self_introduction: 'I like programming and i ca help anyone need it', 
+gender: 'male', skills: 'ruby, javascript', age:'25', qualification: 'A0 in computer scince', password: '123456')
