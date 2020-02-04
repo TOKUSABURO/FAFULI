a3a89698032642d2f7767ba424744b6f9fd83aee
Your Name <you@example.com>
2020-02-04 14:36:28 +0900

    commit message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_083419) do
+ActiveRecord::Schema.define(version: 2020_01_29_121150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,13 +74,7 @@ ActiveRecord::Schema.define(version: 2020_01_31_083419) do
   end
 
   create_table "purchases", force: :cascade do |t|
-    t.string "email"
     t.integer "ammount"
-    t.string "description"
-    t.string "currency"
-    t.string "card"
-    t.string "user_id"
-    t.string "course_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "uuid"
@@ -110,8 +104,8 @@ ActiveRecord::Schema.define(version: 2020_01_31_083419) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
-    t.string "provider"
     t.string "uid"
+    t.string "provider"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
