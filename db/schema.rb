30c14e4b2f8a10979f7fc45d0c5094c7590a7242
Your Name <you@example.com>
2020-02-08 19:51:11 +0900

    change config

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_01_29_121150) do
-=======
 ActiveRecord::Schema.define(version: 2020_01_31_131232) do
->>>>>>> b3b80d800978f196d266b0761dbbb7f23b19ebdd
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,9 +74,6 @@ ActiveRecord::Schema.define(version: 2020_01_31_131232) do
   end
 
   create_table "purchases", force: :cascade do |t|
-<<<<<<< HEAD
-    t.integer "ammount"
-=======
     t.string "email"
     t.string "description"
     t.string "currency"
@@ -89,7 +82,6 @@ ActiveRecord::Schema.define(version: 2020_01_31_131232) do
     t.integer "course_id"
     t.integer "ammount"
     t.string "uuid"
->>>>>>> b3b80d800978f196d266b0761dbbb7f23b19ebdd
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
